8ca7e9cefb974d3673a93808329b5ad49b4f16e3
genildoburgos <gnldburgos@gmail.com>
2025-09-21 16:00:46 -0300

    fix: correct repository ID and update backend configuration in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,11 @@
+#reference: https://cloud.google.com/docs/terraform/best-practices/operations?hl=pt-br
+
 terraform {
+  backend "gcs" {
+    bucket  = "terraform-state-bucket-sauter"
+    prefix  = "artifact-registry" # reference: https://cloud.google.com/storage/docs/terraform-create-bucket-upload-object?hl=pt-br
+  }
+
   required_providers {
     google = {
       source = "hashicorp/google"
@@ -15,6 +22,6 @@ provider "google" {
 resource "google_artifact_registry_repository" "artifactory_repository" {
   project       = "sauter-university-472416"
   location      = "southamerica-east1"
-  repository_id = "docker-artifact-regsitry"
+  repository_id = "docker-artifact-registry"
   format        = "DOCKER"
 }
