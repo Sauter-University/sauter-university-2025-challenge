2f2f5b5ca4602d91ff3976fc4ef2e3a387abcd8a
genildoburgos <gnldburgos@gmail.com>
2025-09-21 17:36:43 -0300

    feat: add initial Terraform configuration and .gitignore for project setup

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+#reference: https://cloud.google.com/docs/terraform/best-practices/operations?hl=pt-br
+
+terraform {
+  # backend "gcs" {
+  #   bucket  = "terraform-state-bucket-sauter"
+  #   prefix  = "artifact-registry" 
+  # }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "sauter-university-472416"
+  region  = "us-central1"
+}
+
+resource "google_artifact_registry_repository" "artifactory_repository" {
+  project       = "sauter-university-472416"
+  location      = "us-central1"
+  repository_id = "docker-artifact-registry"
+  format        = "DOCKER"
+}
