2add33a30c14829db3c1d3af207915eacf4747df
genildoburgos <gnldburgos@gmail.com>
2025-09-22 00:13:46 -0300

    feat: add initial Terraform configuration for Google Cloud services

---
+++ terraform/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "sauter-university-472416"
+  region  = "us-central1"
+}
+
+# Enables the Cloud Run API
+resource "google_project_service" "run_api" {
+  service = "run.googleapis.com"
+
+  disable_on_destroy = true
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "run_all_users" {
+  service  = google_cloud_run_service.default.name
+  location = google_cloud_run_service.default.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+output "service_url" {
+  value = google_cloud_run_service.default.status[0].url
+}
+
+resource "google_artifact_registry_repository" "artifactory_repository" {
+  project       = "sauter-university-472416"
+  location      = "us-central1"
+  repository_id = "docker-artifact-regsitry"
+  format        = "DOCKER"
+}
