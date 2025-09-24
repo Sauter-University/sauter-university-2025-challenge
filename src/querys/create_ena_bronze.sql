46a253c39fa57fc4615c4a35240184db4db7fc13
Gustavo Ferreira Wanderley <mestreguga24@gmail.com>
2025-09-23 23:01:41 -0300

    feat: create a raw data staging table in bigquery

---
+++ src/querys/create_ena_bronze.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE TABLE sauter-university-472416.ons_bronze.ena_basin_bronze AS
+SELECT
+  nom_bacia,
+  ena_data,
+  ena_bruta_bacia_mwmed,
+  ena_bruta_bacia_percentualmlt,
+  ena_armazenavel_bacia_mwmed,
+  ena_armazenavel_bacia_percentualmlt,
+  CURRENT_TIMESTAMP() AS data_carga_bronze
+FROM
+  sauter-university-472416.ons_dataset.external_table
